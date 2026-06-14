338fa456fcfb0c803bca66123a993a3fd185a032
Farqil <farrel.aqilla.nov@gmail.com>
2026-06-14 20:03:18 +0700

    fix voice bugs

--- client.py
+++ client.py
@@ -869,7 +869,6 @@ def run_gui(args):
             # 3. Input → aksi protokol
             running = input_handler.handle(pygame.event.get(), state, net, voice)
 
-            # FIX: Hentikan dan hancurkan koneksi Voice lama jika kita kembali ke layar Login/Menu
             if state.phase == "CONNECT" and voice is not None:
                 voice.close()
                 voice = None
